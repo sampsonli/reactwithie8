474c43e9a7425f210bdf7756333a2125c37f45f5
sampson Li <740056710@qq.com>
2018-05-15 16:51:21 +0800

    fixbug

--- src/common/config.js
+++ src/common/config.js
@@ -4,12 +4,12 @@ if (process.env.NODE_ENV === 'production') {
     if (process.env.EWT_ENV === '235') {
 
 
-        baseURL = 'http://10.0.11.86:8768/api/evaluation';
+        baseURL = 'http://10.0.11.86:8769/api/evaluation';
         loginUrl = 'http://my.235.mistong.com/sso?sid=14'
         // loginUrl = 'http://xinli.ewt360.com/Login?fromurl=http%3a%2f%2fxinli.ewt360.com%2f'
     } else {
         // 线上接口
-        baseURL = 'http://10.0.11.86:8768/api/evaluation';
+        baseURL = 'http://10.0.11.86:8769/api/evaluation';
         loginUrl = 'http://passport.ewt360.com/sso?sid=16'
 
     }
