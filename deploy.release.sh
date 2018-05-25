979d2b0d09ad29768673e32228b4de5aac397834
sampson Li <740056710@qq.com>
2018-05-25 11:06:20 +0800

    optimate

--- deploy.release.sh
+++ deploy.release.sh
@@ -2,8 +2,8 @@
 uri=attainment/evalpc
 pname=evaluation_pc.tag.gz
 yarn
-yarn run build:235
+yarn run build
 cd dist
 tar -zcf $pname *
-scp $pname frontend@10.0.11.59:/home/wwwroot/frontend/tmp/
-ssh frontend@10.0.11.59 "cd /home/wwwroot/frontend/ && rm -rf $uri && mkdir -p $uri && tar zxf tmp/$pname -C $uri"
+scp $pname frontend@10.0.11.59:/home/wwwroot/frontend_release/tmp/
+ssh frontend@10.0.11.59 "cd /home/wwwroot/frontend_release/ && rm -rf $uri && mkdir -p $uri && tar zxf tmp/$pname -C $uri"
