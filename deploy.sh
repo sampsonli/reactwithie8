ecc08f6b59179b80e10ab0480cccc9dd0e2f988b
sampson Li <740056710@qq.com>
2018-05-25 12:30:22 +0800

    fixbug

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,13 @@ pname="evaluation_pc.tar.gz"
 yarn
 pwd
 if [ "$1"x = "235"x ]
+then
     yarn run build:235
     cd dist
     tar -zcf $pname *
     scp $pname frontend@10.0.11.59:/home/wwwroot/frontend/tmp/
     ssh frontend@10.0.11.59 "cd /home/wwwroot/frontend/ && rm -rf $uri && mkdir -p $uri && tar zxf tmp/$pname -C $uri"
-then
+else
     yarn run build
     cd dist
     tar -zcf $pname *
