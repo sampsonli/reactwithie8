b5014939349c855196df0880963c868300baa783
sampson Li <740056710@qq.com>
2018-08-02 15:40:02 +0800

    fixbug

--- src/modules/mobile/index.js
+++ src/modules/mobile/index.js
@@ -3,20 +3,25 @@ import React, {Component, PropTypes} from 'react';
 import {bindActionCreators} from 'redux';
 import {injectReducer} from '~/route';
 import css from './style/index.css';
+
+// //////////////////下面是每个模块入口必须配置的内容，注入reducer的逻辑， 不需要做任何改动\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 import reducers from './reducers';
 import {mid} from './route';
-
-// 自动注入当前模块的 reducer
 injectReducer({key: mid, reducer: reducers});
+// \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\上面是每个模块入口必须配置的内容， 不需要做任何改动///////////////////////////
+
+import {GETINFO}  from './actions/types'
 
 
-@connect(state => ({home: state[mid]}), dispatch => bindActionCreators({}, dispatch))
+@connect(state => ({ewt: state[mid].ewt}), dispatch => bindActionCreators({dispatch}, dispatch))
 export default class Main extends Component {
-
+    handClick = () => {
+        this.props.dispatch({type: GETINFO, payload:  Math.floor(Math.random()*1000000)})
+    }
     render() {
         return (<div className={css.main}>
-            <div className={css.header}></div>
-            <div className={css.content}>移动端页面</div>
+            <div className={css.header} onClick={this.handClick}>点击改变内容</div>
+            <div className={css.content} style={{background: `#${this.props.ewt.rgb}`}}>{this.props.ewt.rgb}</div>
 
         </div>);
     }
--- src/modules/mobile/reducers/ewt.js
+++ src/modules/mobile/reducers/ewt.js
@@ -1,3 +1,16 @@
-export default (state = {name: '你好'}, action) => {
+import {GETINFO} from '../actions/types';
+const initState = {
+    rgb: '2234bb'
+}
+const mutiations = {
+    [GETINFO](state, payload) {
+        state.rgb = payload
+    }
+}
+export default (state = initState, action) => {
+    if(Object.hasOwnProperty.call(mutiations, action.type)) {
+        const newState = {...state};
+        return mutiations[action.type](newState, action.payload) || newState
+    }
     return state;
 }
