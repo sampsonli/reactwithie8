5342d1522823460766077162987dedd7275cfb06
sampson Li <740056710@qq.com>
2018-08-02 16:14:48 +0800

    fixbug

--- src/modules/mobile/reducers/ewt.js
+++ src/modules/mobile/reducers/ewt.js
@@ -2,15 +2,15 @@ import {GETINFO} from '../actions/types';
 const initState = {
     rgb: '2234bb'
 }
-const mutiations = {
+const mutations = {
     [GETINFO](state, payload) {
         state.rgb = payload
     }
 }
 export default (state = initState, action) => {
-    if(Object.hasOwnProperty.call(mutiations, action.type)) {
+    if(Object.hasOwnProperty.call(mutations, action.type)) {
         const newState = {...state};
-        return mutiations[action.type](newState, action.payload) || newState
+        return mutations[action.type](newState, action.payload) || newState
     }
     return state;
 }
