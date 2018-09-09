dad4171f98207261af714d031a2f779a354cc1f9
sampson Li <740056710@qq.com>
2018-09-09 14:23:29 +0800

    优化包

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 
 const React = require('react');
 const ReactDOM = require('react-dom');
-const AppRoute = require('./route').default;
+const AppRoute = require('./router').default;
 
 ReactDOM.render(
     <AppRoute />,
