7462aa1be965f4b0977c7a62729cf84036c8ab8b
sampson Li <740056710@qq.com>
2018-09-18 14:57:30 +0800

    优化包

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-
-const React = require('react');
-const ReactDOM = require('react-dom');
-const AppRoute = require('./router');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppRoute from './router';
 
 ReactDOM.render(
     <AppRoute />,
--- src/store/store.dev.js
+++ src/store/store.dev.js
@@ -17,7 +17,7 @@ const store = createStore(
 if (module.hot) {
     // Enable Webpack hot module replacement for reducers
     module.hot.accept('../reducers', () => {
-        const nextRootReducer = require('../reducers').default;
+        const nextRootReducer = require('../reducers');
         store.replaceReducer(nextRootReducer);
     });
 }
