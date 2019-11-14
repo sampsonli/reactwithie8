12caed74525c543c429b38cedd5cc654c4fa26a0
lichun <740056710@qq.com>
2019-11-14 10:39:03 +0800

    optimate

--- src/routes/demo/views/home/index.js
+++ src/routes/demo/views/home/index.js
@@ -9,24 +9,24 @@ class Home extends Component {
     componentDidMount() {
         store.getInitData();
     }
+
     render() {
         return (
-            <div className={classNames('l-full l-flex-column', css.container)}>
+            <div className={css.container}>
                 <div className={css.header}>
                     <div className={css['h-ct']} onClick={() => store.getNumber()}>
                         <span className={css['h-title']}>demo演示deliverer</span>
                     </div>
                 </div>
-                <div className="l-flex-1 l-relative">
-                    <div className="l-full l-scroll-y">
-                        <div className={css.init}>{store.initData}</div>
-                        <div className={css.button} onClick={store.getNumber}>调用getNumber方法</div>
-                        <div className={css.number}>当前number值： <i>{store.number || '加载中...'}</i></div>
-                    </div>
+                <div className={css.content}>
+                    <div className={css.init}>{store.initData}</div>
+                    <div className={css.button} onClick={store.getNumber}>调用getNumber方法</div>
+                    <div className={css.number}>当前number值： <i>{store.number || '加载中...'}</i></div>
                 </div>
 
             </div>
         );
     }
 }
+
 export default connect(state => ({state: state[store.ns]}))(Home);
